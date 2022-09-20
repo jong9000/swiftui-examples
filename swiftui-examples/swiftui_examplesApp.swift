e7413cd1b8e36f52de14784f91642f8882d08579
Jon Gentry <jonmarkgentry@gmail.com>
2022-09-19 23:49:03 -0700

    Initial Commit

---
+++ swiftui-examples/swiftui_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_examplesApp.swift
+//  swiftui-examples
+//
+//  Created by Jonathan Gentry on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
