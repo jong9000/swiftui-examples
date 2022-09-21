db946ad870b1789f1a6348fb695ee453bf9a6ac6
Jon Gentry <jonmarkgentry@gmail.com>
2022-09-20 17:43:33 -0700

    Add font sizes view

--- swiftui-examples/ContentView.swift
+++ swiftui-examples/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Group {
+                Text("Default")
+            }
+            Group {
+                Text("Large Title")
+                    .font(.largeTitle)
+                Text("Title")
+                    .font(.title)
+                Text("Title 2")
+                    .font(.title2)
+                Text("Title 3")
+                    .font(.title3)
+            }
+            Group {
+                Text("Headline")
+                    .font(.headline)
+                Text("Sub Headlline")
+                    .font(.subheadline)
+                Text("Body")
+                    .font(.body)
+                Text("callout")
+                    .font(.callout)
+                Text("footnote")
+                    .font(.footnote)
+                Text("caption")
+                    .font(.caption)
+                Text("caption2")
+                    .font(.caption2)
+            }
         }
-        .padding()
     }
 }
 
